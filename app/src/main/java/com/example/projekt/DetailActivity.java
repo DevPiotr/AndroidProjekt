dba0fe1a617dcc99d11b2a409b6d8847ac1157e9
piotrdev <piotr.zukiewicz@o2.pl>
2019-05-04 00:38:40 +0200

    Dodanie Aktywności i klasy głównej Zmien sposób trzymania wartości dla posiłków

--- app/src/main/java/com/example/projekt/DetailActivity.java
+++ app/src/main/java/com/example/projekt/DetailActivity.java
@@ -1,14 +1,13 @@
 package com.example.projekt;
 
-import android.app.ListActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends ListActivity {
+public class DetailActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_detail);
     }
 }
---
+++ app/src/main/java/com/example/projekt/MyAplication.java
@@ -0,0 +1,6 @@
+package com.example.projekt;
+
+import android.app.Application;
+
+public class MyAplication extends Application {
+}
